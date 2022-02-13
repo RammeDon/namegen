b594b530d941db23d9f763243938c355ecf58ed0
RammeDon <83227587+RammeDon@users.noreply.github.com>
2022-02-13 20:11:57 +0100

    Add files via upload

--- namegen.js
+++ namegen.js
@@ -15,31 +15,21 @@ function generateUserName(name) {
 function run() {
 
 
-    
-    if (answer != "0"){
-            
-        readline.question('Please enter your name to reveice username options: ', name =>{
-            if (name = "0"){
-                process.exit(1)
-            }
-            for (let nb_name = 0; nb_name < 5; nb_name++){
-                console.log(generateUserName(name));
-            }
-            readline.close();
-        });
+    readline.question('Please enter your name to reveice username options: ', name =>{
 
-    }
-    else{
-        condition = false;
-    }
+        for (let nb_name = 0; nb_name < 5; nb_name++){
+            console.log(generateUserName(name));
+        };
+        
+    });
 
 }
 
 function start(){
     console.log("This app is a random username generator!");
-    console.log("enter 0 anytime to quit");
-    run();
+    console.log("enter 0 to quit");
 
+    run();
 }
 
 exports.start = start;
